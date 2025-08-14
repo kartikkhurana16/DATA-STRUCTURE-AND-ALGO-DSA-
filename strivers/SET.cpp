a7445624dd9253f64cd508c088bda31af4316ca8
kartikkhurana16 <kartikkhurana136@gmail.com>
2025-08-15 00:31:22 +0530

    STL

--- strivers/SET.cpp
+++ strivers/SET.cpp
@@ -10,14 +10,16 @@ void start()
     
     for(auto it:s)
     {
-    cout<<it<<endl;
+    cout<<it<<" ";
     }
     // auto it=s.find(11);
     // cout<<*it;
     auto it=s.find(10);
-    cout<<*it<<endl;
-
-    s.erase(10);
+    cout<<*it;
+    cout<<endl;
+    s.upper_bound(2);
+    // s.erase(10);
+    s.upper_bound(2);
     for(auto it:s)
     {
     cout<<it<<endl;
