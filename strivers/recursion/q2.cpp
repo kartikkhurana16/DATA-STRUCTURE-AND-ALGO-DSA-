2ec574297d2772f5276ccc7a1bcf8ebc39842574
kartikkhurana16 <kartikkhurana136@gmail.com>
2025-08-23 00:58:19 +0530

    q2

--- strivers/recursion/q2.cpp
+++ strivers/recursion/q2.cpp
@@ -1,11 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-void start()
+int i=1;
+
+void start(int n)
 {
-    
+    if(i==n)
+    {
+        return;
+    }
+    cout<<i;
+    i++;
+    start(n);
 }
 int main()
 {
-    start();
+    int n;
+    cout<<"enter how many number u want to print";
+    cin>>n;
+    start(n);
     return 0;
 }
