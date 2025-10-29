8a4011e7047fe64a04ecd981d99540f1ed98f9a9
kartikkhurana16 <kartikkhurana136@gmail.com>
2025-10-30 01:35:05 +0530

    10 question

--- strivers/question/10.cpp
+++ strivers/question/10.cpp
@@ -2,14 +2,27 @@
 using namespace std;
 int show(vector<int>&arr,int n)
 {
+    
     for(int i=0;i<=n;i++)
     {
-        if()
+        int  flag=0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]==i)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+        {
+            cout<<i<<" ";
+        }
     }
 }
 int main()
 {
-    vector<int>arr={1,2,3,4,5};
+    vector<int>arr={1,3,4,5};
     int n;
     cin>>n;
     show(arr,n);
